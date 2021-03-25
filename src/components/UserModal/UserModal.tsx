103d8827f0c18548bd886e20c81bb63d73db6f30
Vladimir Abrahamyan <vladimir.abrahamyan96@gmail.com>
2021-03-25 18:33:18 +0400

    Change modal classname

--- src/components/UserModal/UserModal.tsx
+++ src/components/UserModal/UserModal.tsx
@@ -1,36 +1,34 @@
-import classes from "./UserModal.module.css";
-import Button from "../Button/Button";
-import React from "react";
+import classes from './UserModal.module.css';
+import Button from '../Button/Button';
+import React from 'react';
 
 interface Props {
-    street:string,
-    company:string,
-    close():void
+    street: string,
+    company: string,
+    close(): void
 }
 
-
-const UserModal = ({street, company, close}:Props) => {
+const UserModal = ({ street, company, close }: Props) => {
     return (
-        <div className={classes['UserModal']}>
-           <div className={classes['container']}>
-               <div className={classes['about']}>
-                   <figure>
-                       <img src="https://dwpdigital.blog.gov.uk/wp-content/uploads/sites/197/2016/07/P1090594-1.jpeg" alt=""/>
-                   </figure>
-                   <div className={classes['content']}>
-                       <p>
-                           <span>Company</span> <br/>
-                           <b>{company}</b>
-                       </p>
-                       <p>
-                           <span>Street</span> <br/>
-                           <b>{street}</b>
-                       </p>
-                   </div>
-
-               </div>
-               <Button onClick={close}> Close </Button>
-           </div>
+        <div className={classes['modal']}>
+            <div className={classes['modal__body']}>
+                <div className={classes['modal__about']}>
+                    <figure>
+                        <img src="https://dwpdigital.blog.gov.uk/wp-content/uploads/sites/197/2016/07/P1090594-1.jpeg" alt="" />
+                    </figure>
+                    <div className={classes['modal__content']}>
+                        <p>
+                            <span>Company</span> <br />
+                            <b>{company}</b>
+                        </p>
+                        <p>
+                            <span>Street</span> <br />
+                            <b>{street}</b>
+                        </p>
+                    </div>
+                </div>
+                <Button onClick={close}> Close </Button>
+            </div>
         </div>
     );
 };
